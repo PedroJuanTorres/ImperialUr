58be69e7e133a61b2e3c887783384f18039f1ab0
DanielPinhao22007445 <daniel2pinhao@gmail.com>
2021-05-16 15:01:31 +0100

    Added CleanScreen and Menu metods

--- ImperialUr/View.cs
+++ ImperialUr/View.cs
@@ -15,5 +15,38 @@ namespace ImperialUr
             this.field = field;
             this.player = player;
         }
+
+
+        public static void CleanScreen ()
+        {
+            Console.Clear ();
+        }
+
+
+        public static string Menu () 
+        {   
+            Console.WriteLine ("\n");
+            Console.WriteLine ("==============================================");
+            Console.WriteLine ("==================== Menu ====================");
+            Console.WriteLine ("==============================================");
+            Console.WriteLine ("                 ============                 ");
+            Console.WriteLine ("                 = New Game =                 ");
+            Console.WriteLine ("                 ============                 ");
+            Console.WriteLine ("                 ============                 ");
+            Console.WriteLine ("                 = Continue =                 ");
+            Console.WriteLine ("                 ============                 ");
+            Console.WriteLine ("                   ========                   ");
+            Console.WriteLine ("                   = Team =                   ");
+            Console.WriteLine ("                   ========                   ");
+            Console.WriteLine ("                   ========                   ");
+            Console.WriteLine ("                   = Quit =                   ");
+            Console.WriteLine ("                   ========                   ");
+            Console.WriteLine ("==============================================");
+            Console.WriteLine ("\n");
+            Console.WriteLine ("Which option would you like to choose?");
+            Console.Write ("\n>> ");
+
+            return (Console.ReadLine ());
+        }
     }
 }
